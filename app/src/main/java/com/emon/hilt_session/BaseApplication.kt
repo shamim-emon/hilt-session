181f40e8ef30d51b43c00782e39b2a6827d3c26e
shamim-emon <emon.dev.bd@gmail.com>
2026-03-31 17:00:03 +0600

    Initial commit

---
+++ app/src/main/java/com/emon/hilt_session/BaseApplication.kt
@@ -0,0 +1,11 @@
+package com.emon.hilt_session
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
